f03ada09ec1d9282606c4ab22fcfba7aeb61dc6f
HartMason <hartmason85@gmail.com>
2022-07-31 22:15:45 -0500

    added methods to class App function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import React, { ReactDOM } from "react";
+import "./App.css";
+import "axios"; 
+import axios from "axios";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+componentDidMount() {                      //method is a function that belongs to a class
+  this.fetchAdvice();
+}
+
+fetchAdvice = () => {
+  axios.get('http://api.adviceslip.com/advice')
+  .then(() => {
+    console.log("response")
+  })
+  .catch(() => {
+    console.log("error")
+  })
+}  
+
+  render() {
+    return <h1>APP</h1>;
+  }
 }
 
 export default App;
